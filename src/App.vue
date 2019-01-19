6daa40c5016b14879527388c661b1b307979d36b
Knut Hühne <knut@k-nut.eu>
2019-01-19 11:04:44 +0100

    add explanatory text, rename project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+      <h1> City-Country-Shapes</h1>
+      <p class="intro"> How many of a country's biggest cities does it take for you to recognize the country by the shape
+      of its cities? You can try around here. Just pick a country and a number of cities to display.</p>
+      <p class="intro disclaimer">This project is based on data from <a href="https://wikidata.org">Wikidata</a>.
+      At times the data might not be complete and you might not get as many cities as you asked for. The great thing
+      about Wikidata is that - just like Wikipedia - it is open for everyone to improve it.
+         You can <a href="https://www.wikidata.org/wiki/Wikidata:Introduction">start here</a> to learn more about the project or
+      get in touch with me on <a href="https://twitter.com/notknut">twitter</a>.</p>
     <div clas="settings">
       <CountryPicker v-on:selected="onCountryChange($event)" v-bind:default-value="country"/>
       <label for="count">
@@ -50,5 +58,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+}
+.intro {
+  max-width: 80ch;
+}
+.disclaimer {
+  font-size: .8rem;
 }
 </style>
